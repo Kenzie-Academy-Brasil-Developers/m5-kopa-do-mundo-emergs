608bea2d1736ff39eda59b988f511c1aec88a1f4
emerson gonçalves <emer_gs@hotmail.com>
2022-11-23 14:56:52 -0300

    feat:conclusão da tarefa 2

--- teams/models.py
+++ teams/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Team(models.Model):
-    id = (models.BigAutoField(primary_key=True),)
-    nome = (models.CharField(max_length=30),)
-    titles = (models.IntegerField(null=True, default=0),)
-    top_scorer = (models.CharField(max_length=50),)
-    fifa_code = (models.CharField(max_length=3, unique=True),)
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    titles = models.IntegerField(null=True, default=0)
+    top_scorer = models.CharField(max_length=50)
+    fifa_code = models.CharField(max_length=3, unique=False)
     founded_at = models.DateField(null=True)
 
     def __repr__(self):
--- teams/views.py
+++ teams/views.py
@@ -1,3 +1,27 @@
+from django.forms.models import model_to_dict
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from teams.models import Team
 
-# Create your views here.
+
+class TeamView(APIView):
+    def post(self, request):
+        team_data = request.data
+        team = Team.objects.create(
+            name=team_data["name"],
+            titles=team_data["titles"],
+            top_scorer=team_data["top_scorer"],
+            fifa_code=team_data["fifa_code"],
+            founded_at=team_data["founded_at"],
+        )
+        return Response(model_to_dict(team), 201)
+
+    def get(self, request):
+        teams = Team.objects.all()
+        teams_dict = []
+
+        for team in teams:
+            teams_dict.append(model_to_dict(team))
+
+        return Response(teams_dict, 200)
