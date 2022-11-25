d4fd98423b57a784ba47fad068229644cd33c0d7
emerson gonçalves <emer_gs@hotmail.com>
2022-11-25 13:25:07 -0300

    feat:projeto concluído

--- teams/urls.py
+++ teams/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from .views import TeamView
+from .views import TeamView, TeamViewId
 
-urlpatterns = [path("teams/", TeamView.as_view())]
+urlpatterns = [
+    path("teams/", TeamView.as_view()),
+    path("teams/<int:id>/", TeamViewId.as_view()),
+]
--- teams/views.py
+++ teams/views.py
@@ -1,6 +1,6 @@
 from django.forms.models import model_to_dict
 from django.shortcuts import render
-from rest_framework.views import APIView
+from rest_framework.views import APIView, status
 from rest_framework.response import Response
 from teams.models import Team
 
@@ -15,7 +15,7 @@ class TeamView(APIView):
             fifa_code=team_data["fifa_code"],
             founded_at=team_data["founded_at"],
         )
-        return Response(model_to_dict(team), 201)
+        return Response(model_to_dict(team), status.HTTP_201_CREATED)
 
     def get(self, request):
         teams = Team.objects.all()
@@ -24,4 +24,40 @@ class TeamView(APIView):
         for team in teams:
             teams_dict.append(model_to_dict(team))
 
-        return Response(teams_dict, 200)
+        return Response(teams_dict, status.HTTP_200_OK)
+
+
+class TeamViewId(APIView):
+    def get(self, request, id: int):
+        try:
+            team = Team.objects.get(pk=id)
+        except Team.DoesNotExist:
+            return Response({"message": "Team not found"}, status.HTTP_404_NOT_FOUND)
+
+        return Response(model_to_dict(team))
+
+    def delete(self, request, id: int):
+        try:
+            team = Team.objects.get(pk=id)
+        except Team.DoesNotExist:
+            return Response({"message": "Team not found"}, status.HTTP_404_NOT_FOUND)
+
+        team.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def patch(self, request, id: int):
+        try:
+            team = Team.objects.get(pk=id)
+        except Team.DoesNotExist:
+            return Response({"message": "Team not found"}, status.HTTP_404_NOT_FOUND)
+
+        team.name = request.data.get("name", team.name)
+        team.titles = request.data.get("titles", team.titles)
+        team.top_scorer = request.data.get("top_scorer", team.top_scorer)
+        team.fifa_code = request.data.get("fifa_code", team.fifa_code)
+        team.founded_at = request.data.get("founded_at", team.founded_at)
+
+        team.save()
+
+        return Response(model_to_dict(team), status.HTTP_200_OK)
